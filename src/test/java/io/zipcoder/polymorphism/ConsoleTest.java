b648df579998014a42795c1f486c9ecb197101f9
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-13 11:16:29 -0500

    added test structure for console

---
+++ src/test/java/io/zipcoder/polymorphism/ConsoleTest.java
@@ -0,0 +1,14 @@
+package io.zipcoder.polymorphism;
+
+import org.junit.Test;
+
+public class ConsoleTest {
+    @Test
+    public void getNumberOfPetsTest(){}
+
+    @Test
+    public void getPetsTest(){}
+
+    @Test
+    public void printListOfPetsTest(){}
+}
